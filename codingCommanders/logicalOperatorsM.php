50dd575a21dfa2d8267b26521260ff397779be45
Caprica0606 <caprica0606@gmail.com>
2017-04-27 10:41:07 -0400

    conditional stuff

---
+++ codingCommanders/logicalOperatorsM.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="../codingCommanders.css">
+  <link rel="stylesheet" type="text/css" href="customLessons.css">
+  <title>Into to PHP: Logical Operators</title>
+  </head>
+  <body >
+    <!-- Header-->
+      <div class = "headContainer">
+        <!-- Top Menu-->
+        <div class = "menuDiv">
+          <span class = "row">
+            <a class = "nav" href = "../codingCommanders.php">Home</a>
+            <a class ="nav" href ="../suppliment.php">Lessons</a>
+            <a class = "nav" href = "https://www.youtube.com/channel/UC-gCUMK_EGUqJ7P9VA7BJmQ">YouTube Channel</a>
+            <a class = "nav" href = "../blog.html">B l o g</a>
+          </span>
+        </div>
+      <!-- Page Tittle-->
+      <div class = "heading">
+        <span class = "row">
+          <div class = "col-6" style = "float: right;">
+            <h1>PHP: Logical Operators</h1>
+         </div>
+          <div class = "col-3" style = "float: left;">
+            <img width = "300px;" src ="../logo.png">
+          </div>
+        </span>
+      </div>
+    </div>
+    <!-- Table-->
+    <div class = "col-12" align = "center">
+      <img src="iffyPics/logicalOperatorsM.PNG">
+    </div>
+  </body>
+<html>
